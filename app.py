44a224cebfcd8e47b83baf3ae29c5cbd9c3b4cda
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-02-02 19:30:43 +0500

    Bug Fixes

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import json
-
-from raven.transport import requests
-
 from crawler.crawler_instance.application_controller.application_controller import application_controller
 from crawler.crawler_instance.application_controller.application_enums import APPICATION_COMMANDS
+
 # mongo_controller.get_instance().invoke_trigger(MONGO_CRUD.S_DELETE,[MONGODB_COMMANDS.S_CLEAR_INDEX,[None],[None]])
 # mongo_controller.get_instance().invoke_trigger(MONGO_CRUD.S_DELETE,[MONGODB_COMMANDS.S_CLEAR_BACKUP,[None],[None]])
 
