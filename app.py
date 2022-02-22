1124644f4b46f90a09991797fb2371210121ed27
Abdul Mannan <abdul.mannan@catchprobe.com>
2022-02-22 21:17:18 +0500

    Bug Fixes
    Bug Fixes

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from crawler.crawler_instance.application_controller.application_controller import application_controller
 from crawler.crawler_instance.application_controller.application_enums import APPICATION_COMMANDS
 
-# application_controller.get_instance().invoke_triggers(APPICATION_COMMANDS.S_START_APPLICATION)
-from crawler.crawler_services.helper_services.spell_check_handler import spell_checker_handler
-
-print(spell_checker_handler.get_instance().stem_word("service"))
+application_controller.get_instance().invoke_triggers(APPICATION_COMMANDS.S_START_APPLICATION)
