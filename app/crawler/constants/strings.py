60319772db530fbaa5ef7fd4c73f795b567429d6
Abdul Mannan Saeed <111820160@umt.edu.pk>
2022-07-15 22:37:13 +0500

    Bug Fixes
    Bug Fixes

--- app/crawler/constants/strings.py
+++ app/crawler/constants/strings.py
@@ -11,8 +11,8 @@ class STRINGS:
 
 
 class TOR_STRINGS:
-    S_SOCKS_HTTP_PROXY = "socks5h://10.0.0.106:"
-    S_SOCKS_HTTPS_PROXY = "socks5h://10.0.0.106:"
+    S_SOCKS_HTTP_PROXY = "socks5h://10.0.0.103:"
+    S_SOCKS_HTTPS_PROXY = "socks5h://10.0.0.103:"
     S_RELEASE_PORT = 'for /f "tokens=5" %a in (\'netstat -aon ^| find "' + str(TOR_CONNECTION_CONSTANTS.S_TOR_CONNECTION_PORT) + '"\') do taskkill /f /pid %a'
 
 
