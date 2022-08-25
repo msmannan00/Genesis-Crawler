dfe003c94002cbfce5e44ae710b50afc0c0f445f
Abdul Mannan Saeed <111820160@umt.edu.pk>
2022-08-25 23:05:36 +0500

    Bug Fixes
    Bug Fixes

--- app/start_celery.sh
+++ app/start_celery.sh
@@ -1,1 +1,1 @@
-celery "-A" "crawler.crawler_instance.genbot_service.genbot_controller" "worker" "--concurrency=50" "--pool=gevent" "--loglevel=info" "-Q" "genbot_queue"
+celery "-A" "crawler.crawler_instance.genbot_service.genbot_controller" "worker" "--concurrency=150" "--pool=gevent" "--loglevel=info" "-Q" "genbot_queue"
--- genesis_startup.bat
+++
@@ -1,3 +0,0 @@
-start msedge https://app.logdna.com/
-cd C:\Workspace\Genesis-Crawler
-docker-compose up --build
