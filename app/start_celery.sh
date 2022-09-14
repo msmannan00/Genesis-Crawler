5786edd88320468b97e000797a54930bb6dd287a
Abdul Mannan Saeed <msmannan00@gmail.com>
2022-09-14 16:22:14 +0500

    Bug Fixes
    Bug Fixes

--- app/start_celery.sh
+++ app/start_celery.sh
@@ -1,1 +1,1 @@
-celery "-A" "crawler.crawler_instance.genbot_service.genbot_controller" "worker" "--concurrency=1" "--pool=gevent" "--loglevel=info" "-Q" "genbot_queue"
+celery "-A" "crawler.crawler_instance.genbot_service.genbot_controller" "worker" "--concurrency=150" "--pool=gevent" "--loglevel=info" "-Q" "genbot_queue"
