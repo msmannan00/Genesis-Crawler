1a1d823debaa214d05a044a585b4f97b404a657f
Abdul Mannan Saeed <msmannan00@gmail.com>
2022-09-15 18:57:13 +0500

    Bug Fixes
    Bug Fixes

--- app/crawler/crawler_instance/genbot_service/genbot_controller.py
+++ app/crawler/crawler_instance/genbot_service/genbot_controller.py
@@ -82,7 +82,7 @@ class genbot_controller(request_handler):
                 self.__m_url_duplication_handler.insert(m_sub_url)
                 m_sub_url_filtered.append(helper_method.on_clean_url(m_sub_url))
 
-        p_parsed_model.m_sub_url = m_sub_url_filtered[0:10]
+        p_parsed_model.m_sub_url = m_sub_url_filtered[0:50]
 
         return p_parsed_model
 
