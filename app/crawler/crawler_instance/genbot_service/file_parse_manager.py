8c137fc87117869b14bef5a8df6f7fa2440c6868
msmannan00 <111820160@umt.edu.pk>
2022-10-22 04:31:53 +0500

    Bug Fixes
    Bug Fixes

--- app/crawler/crawler_instance/genbot_service/file_parse_manager.py
+++ app/crawler/crawler_instance/genbot_service/file_parse_manager.py
@@ -1,16 +1,13 @@
 # Local Imports
 import copy
 import json
-import os
 import random
 import string
 import time
 
-from PIL import Image
 from gevent import sleep
-from crawler.constants.constant import CRAWL_SETTINGS_CONSTANTS, RAW_PATH_CONSTANTS
+from crawler.constants.constant import CRAWL_SETTINGS_CONSTANTS
 from crawler.constants.strings import PARSE_STRINGS, MANAGE_CRAWLER_MESSAGES
-from crawler.crawler_instance.helper_services.helper_method import helper_method
 from crawler.crawler_instance.local_shared_model.image_model import image_model_init, image_model_list, image_model
 from crawler.crawler_instance.genbot_service.web_request_handler import webRequestManager
 from crawler.crawler_instance.local_shared_model.unique_file_model import unique_file_model
@@ -18,8 +15,6 @@ from crawler.crawler_instance.local_shared_model.url_model import url_model
 from crawler.crawler_services.helper_services.duplication_handler import duplication_handler
 from crawler.crawler_shared_directory.log_manager.log_controller import log
 from crawler.shared_data import celery_shared_data
-from libs.nudenet.lite_classifier import LiteClassifier
-
 
 class file_parse_manager:
     __m_duplication_url_handler = None
@@ -83,12 +78,11 @@ class file_parse_manager:
         m_porn_image_count = 0
         m_list_temp = copy.copy(p_list)
 
-        while len(m_list_temp) > 0:
+        while len(m_list_temp[0:30]) > 0:
             try:
                 if celery_shared_data.get_instance().get_network_status():
                     m_url = m_list_temp.__getitem__(0)
-                    if (p_request_model.m_depth == 0 and len(m_filtered_list) >= 15) or (
-                            p_request_model.m_depth == 1 and len(m_filtered_list) >= 5) or m_porn_image_count > 5:
+                    if len(m_filtered_list) >= 5:
                         break
 
                     if self.__m_duplication_url_handler.validate_duplicate(m_url) is False:
@@ -111,44 +105,26 @@ class file_parse_manager:
 
                             m_content_type = m_response.headers['Content-Type'].split('/')[0]
                             m_file_type = m_response.headers['Content-Type'].split('/')[1]
-                            m_url_path = key + "." + m_response.headers['Content-Type'].split('/')[1]
+                            # m_url_path = key + "." + m_response.headers['Content-Type'].split('/')[1]
 
-                            if len(m_file_type) > 4 or m_file_type == "gif" or m_content_type != "image" or len(
-                                    m_response.content) < 15000 or " html" in str(m_response.content):
+                            if len(m_file_type) > 4 or m_file_type == "gif" or m_content_type != "image" or len(m_response.content) < 10000:
                                 m_list_temp.pop(0)
                                 continue
 
-                            m_url_path = RAW_PATH_CONSTANTS.S_CRAWLER_IMAGE_CACHE_PATH + m_url_path
-                            helper_method.write_content_to_path(m_url_path, m_response.content)
-                            m_classifier = LiteClassifier()
-                            m_classifier_response = m_classifier.classify(m_url_path)
+                            self.__m_images[m_url] = 'g'
+                            m_filtered_list.append(image_model_init(m_url, 'g'))
+                            m_filtered_list_unique.append(json.loads(json.dumps(image_model_init(m_url, 'g').dict())))
 
-                            width, height = Image.open(m_url_path).size
-                            if width < 250 and height < 250:
-                                os.remove(m_url_path)
-                                m_list_temp.pop(0)
-                                continue
-
-                            log.g().s(MANAGE_CRAWLER_MESSAGES.S_FILE_PARSED + " : " + m_url)
-                            if m_classifier_response[m_url_path]['unsafe'] > 0.5:
-                                m_porn_image_count += 1
-                                self.__m_images[m_url] = 'a'
-                                m_filtered_list.append(image_model_init(m_url, 'a'))
-                                m_filtered_list_unique.append(json.loads(json.dumps(image_model_init(m_url, 'a').dict())))
-
-                            else:
-                                self.__m_images[m_url] = 'g'
-                                m_filtered_list.append(image_model_init(m_url, 'g'))
-                                m_filtered_list_unique.append(json.loads(json.dumps(image_model_init(m_url, 'g').dict())))
-
-                            os.remove(m_url_path)
+                            # os.remove(m_url_path)
                             self.__m_duplication_url_handler.insert(m_url)
+                            log.g().s(MANAGE_CRAWLER_MESSAGES.S_FILE_PARSED + " : " + m_url)
 
                     elif m_url in self.__m_images:
                         m_filtered_list.append(image_model_init(m_url, self.__m_images[m_url]))
                 else:
                     sleep(30)
                     continue
+                sleep(5)
                 m_list_temp.pop(0)
             except Exception as ex:
                 log.g().e(str(ex))
