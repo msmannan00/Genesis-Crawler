a6cb2d6495c4d01bb35833e048bea7c65dac09d8
Abdul Mannan Saeed <111820160@umt.edu.pk>
2022-10-24 16:14:39 +0500

    Bug FIxes
    Bug FIxes

--- app/crawler/crawler_services/crawler_services/elastic_manager/elastic_controller.py
+++ app/crawler/crawler_services/crawler_services/elastic_manager/elastic_controller.py
@@ -2,6 +2,8 @@
 import json
 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
 
 from crawler.constants.strings import MANAGE_ELASTIC_MESSAGES, MANAGE_CRAWLER_MESSAGES
 from crawler.crawler_services.crawler_services.elastic_manager.elastic_enums import ELASTIC_CONNECTIONS
@@ -23,23 +25,31 @@ class elastic_controller(request_handler):
         elastic_controller.__instance = self
 
     def __post_data(self, p_commands, p_data):
-        try:
-            m_json_data = json.dumps(p_data)
-            m_post_object = {'pRequestCommand': p_commands, "pRequestData": m_json_data}
-            m_response = requests.post(ELASTIC_CONNECTIONS.S_DATABASE_IP, data=m_post_object)
-            m_data = json.loads(m_response.text)
-            m_status = m_data[0]
-            m_data = m_data[1]
-            if not m_status:
-                log.g().e(MANAGE_ELASTIC_MESSAGES.S_REQUEST_FAILURE + " : " + str(m_data))
-            elif m_data:
-                log.g().s(MANAGE_ELASTIC_MESSAGES.S_REQUEST_SUCCESS + " : " + str(m_data))
-                m_data = m_data['hits']['hits']
-            return m_status, m_data
-        except Exception as ex:
-
-            log.g().e(MANAGE_CRAWLER_MESSAGES.S_ELASTIC_ERROR + " : " + str(ex))
-            return False, None
+        m_counter = 0
+        while True:
+            try:
+                m_json_data = json.dumps(p_data)
+                m_post_object = {'pRequestCommand': p_commands, "pRequestData": m_json_data}
+                session = requests.Session()
+                retry = Retry(connect=3, backoff_factor=0.5)
+                adapter = HTTPAdapter(max_retries=retry)
+                session.mount('http://', adapter)
+                session.mount('https://', adapter)
+                m_response = session.post(ELASTIC_CONNECTIONS.S_DATABASE_IP, data=m_post_object)
+                m_data = json.loads(m_response.text)
+                m_status = m_data[0]
+                m_data = m_data[1]
+                if not m_status:
+                    log.g().e(MANAGE_ELASTIC_MESSAGES.S_REQUEST_FAILURE + " : " + str(m_data))
+                elif m_data:
+                    log.g().s(MANAGE_ELASTIC_MESSAGES.S_REQUEST_SUCCESS + " : " + str(m_data))
+                    m_data = m_data['hits']['hits']
+                return m_status, m_data
+            except Exception as ex:
+                m_counter+=1
+                log.g().e(MANAGE_CRAWLER_MESSAGES.S_ELASTIC_ERROR + " : " + str(ex))
+                if m_counter>5:
+                    return False, None
 
     def invoke_trigger(self, p_commands, p_data=None):
         return self.__post_data(p_commands, p_data)
